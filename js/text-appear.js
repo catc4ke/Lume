ce4a45c5f2c82a4642d1bebf47551e95ecce0c8b
catc4ke <julianjenkinson@hotmail.com>
2025-02-24 13:24:12 +1100

    no comment

--- js/text-appear.js
+++ js/text-appear.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const slider = document.querySelectorAll(".about-text, .line-1, .line-2, .line-3, .line-4, .about-heading");
+    const slider = document.querySelectorAll(".about-text, .line-1, .line-2, .line-3, .line-4, .about-heading, .tagline-heading");
 
     function handleScroll() {
         slider.forEach((line) => {
             const rect = line.getBoundingClientRect();
-            if (rect.top < window.innerHeight * 0.90 && !line.classList.contains("text-appear")) {
+            if (rect.top < window.innerHeight * .95 && !line.classList.contains("text-appear")) {
                 line.classList.add("text-appear"); // Animate slide
             }
         });
